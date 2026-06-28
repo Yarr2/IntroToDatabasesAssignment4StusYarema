b594612f8401cbb93575649a94d2fc4e3b67a515
Yarema Stus <151072658+Yarr2@users.noreply.github.com>
2026-06-28 16:59:25 +0300

    added roles

---
+++ roles.sql
@@ -0,0 +1,9 @@
+/* giving roles  */
+
+create role administrator with password 'real-password';
+create role developer with password 'repoleved';
+create role analyst with password 'very-smart';
+
+grant all privileges on database steam_clone to administrator;
+grant select on database steam_clone to analyst; /*so they only could interact with data not change it*/
+grant connect, update, delete, insert on database steam_clone to developer; /* so they can add games,users and other stuff to database and check results afterwards*/ 
