64e9db8c4adb6e0f494e85240284f5fa615498ee
Yarema Stus <151072658+Yarr2@users.noreply.github.com>
2026-07-03 17:04:25 +0300

    added permision to developer in roles.sql

--- roles.sql
+++ roles.sql
@@ -6,4 +6,4 @@ create role analyst with password 'very-smart';
 
 grant all privileges on database steam_clone to administrator;
 grant select on database steam_clone to analyst; /*so they only could interact with data not change it*/
-grant connect, update, delete, insert on database steam_clone to developer; /* so they can add games,users and other stuff to database and check results afterwards*/ 
+grant connect, select ,update, delete, insert on database steam_clone to developer; /* so they can add games,users and other stuff to database and check results afterwards*/ 
