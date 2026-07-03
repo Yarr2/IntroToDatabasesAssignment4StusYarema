2e4ac7ce844127f50cd4472993178a586e22d1e0
Yarema Stus <151072658+Yarr2@users.noreply.github.com>
2026-07-03 17:50:10 +0300

    Enhance buy_game procedure with transaction logic
    Updated buy_game procedure to include transaction handling and user_games_id assignment.

--- procedure_trigger.sql
+++ procedure_trigger.sql
@@ -3,7 +3,12 @@
 create or replace procedure buy_game(user_id_p int, game_id_p int)
 language plpgsql
 as $$
+declare
+	v_next_user_games_id int;
+    v_game_price numeric(10,2);
+    v_next_transaction_id int;
 begin
+
 	if (user_id_p not in (select u.user_id from users u)) then 
 		raise exception 'There is no user with id %', user_id_p;
 	end if;
@@ -11,11 +16,20 @@ begin
 	if (game_id_p not in (select g.game_id from games g)) then
 		raise exception 'There is no game with id %', game_id_p;
 	end if;
+		
+	select coalesce(max(user_games_id), 0) + 1 into v_next_user_games_id from user_games;
 
-	insert into user_games (user_id, game_id)
-	values (user_id_p, game_id_p);
+	insert into user_games (user_games_id, user_id, game_id)
+	values (v_next_user_games_id, user_id_p, game_id_p);
+
+    select price into v_game_price from games where game_id = game_id_p;
+    select coalesce(max(transaction_id), 0) + 1 into v_next_transaction_id from transactions;
+        
+    insert into transactions (transaction_id, user_id, amount, card_details)
+    values (v_next_transaction_id, user_id_p, v_game_price, 'Wallet Balance');
 end;
 $$;
+
 
 /*triger*/
 create or replace function insert_user_games_data()
